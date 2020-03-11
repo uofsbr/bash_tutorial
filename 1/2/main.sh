9b02a6b25c36399b9ebbd2e9ddefc40db958fa38
Ulisses Oliva <uofs.br@gmail.com>
2020-03-11 18:13:43 -0300

    Adding Item 2

---
+++ 1/2/main.sh
@@ -0,0 +1,16 @@
+# 02 - USING VARIABLES AND COMMENTS
+
+# SYSTEM VARIABLES
+echo $BASH
+#/bin/bash
+
+echo $BASH_VERSION
+# 4.4.20(1)-release
+
+echo $HOME
+# /home/username
+
+# USED DEFINED VARIABLES
+name=Jhon
+echo My name is $name
+# My name is Jhon
